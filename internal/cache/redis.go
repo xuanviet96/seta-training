4c9b6da4d05693ef58d6fdbe4f87ca36e4f727de
nguyenvietxuan96@gmail.com <nguyenvietxuan96@gmail.com>
2025-09-17 00:31:00 +0700

    add redis cache

---
+++ internal/cache/redis.go
@@ -0,0 +1,35 @@
+package cache
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"go_blog/internal/config"
+
+	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
+)
+
+func New(cfg config.Config, log *zap.Logger) (*redis.Client, error) {
+	if cfg.RedisAddr == "" {
+		return nil, errors.New("REDIS_ADDR empty")
+	}
+	rdb := redis.NewClient(&redis.Options{
+		Addr: cfg.RedisAddr,
+		DB:   cfg.RedisDB,
+	})
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		return nil, err
+	}
+	log.Info("connected to redis", zap.String("addr", cfg.RedisAddr))
+	return rdb, nil
+}
+
+func TTL(cfg config.Config) time.Duration {
+	ttl := time.Duration(cfg.RedisTTLSeconds) * time.Second
+	if ttl <= 0 {
+		return 5 * time.Minute
+	}
+	return ttl
+}
